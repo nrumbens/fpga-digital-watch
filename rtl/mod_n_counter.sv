6f08e8dbe1b753a9d92ee39640057a715f87f85e
Tash <nrumbens@gmail.com>
2026-05-06 13:24:04 +0000

    added modulo n counter

---
+++ rtl/mod_n_counter.sv
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+
+module mod_n_counter #(
+    parameter int N = 4,
+    parameter int WIDTH = 2
+) (
+    input logic clk,
+    input logic rst,
+    input logic enable,
+    output logic [WIDTH - 1:0] count
+);
+
+  localparam logic [WIDTH-1:0] Max = WIDTH'(N);
+  initial count = '0;
+  logic [WIDTH - 1:0] next_count;
+
+  always_ff @(posedge clk)
+    if (rst) count <= '0;
+    else if (enable) count <= next_count;
+
+  always_comb begin
+    next_count = (count < Max - WIDTH'(1)) ? count + WIDTH'(1) : '0;
+  end
+
+endmodule
